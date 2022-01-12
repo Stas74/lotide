f87b98501efd3d95b0f7e88f4b17b22cf26207ff
Stas74 <splotnikov74@gmail.com>
2022-01-11 21:57:51 -0700

    eqArrays file to compare arrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅' + '✅' + '✅' + 'Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🛑' + '🛑' + '🛑' + 'Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -12,3 +12,13 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Bootcamp", "Bootcamp");
 assertEqual(1, 2);
+
+/*
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅' + '✅' + '✅' + 'Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑' + '🛑' + '🛑' + 'Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+*/
---
+++ reverse.js
@@ -0,0 +1,14 @@
+const args = process.argv;
+
+newstring += process.argv[j]
+
+const reverse = function (line) {
+  let result = '';
+  for (let i = line.length-1; i >= 0; i--) {
+    result += line[i];
+  }
+  return result;
+}
+
+console.log(reverse('hello'));
+
