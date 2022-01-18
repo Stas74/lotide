77e84ebc6b36a164dceab8c2d6e290824785ca02
Stas74 <splotnikov74@gmail.com>
2022-01-17 18:29:32 -0700

    Remove test code from file

--- middle.js
+++ middle.js
@@ -1,32 +1,6 @@
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false
-  }
-
-  for (let i = 0; i < arrOne.length; i++) {    
-    if (arrOne[i] !== arrTwo[i]) {
-      return false
-    }
-  }
-  return true
-}
-
-const  assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-    return true
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false
-  }
-}
-
 const middle = function(array) {
   //...
   return array.slice(1, -1);
-}
+};
 
-
-
-
-console.log(middle(["bison", "camel", "duck", "elephant"]));
+module.exports = middle;
