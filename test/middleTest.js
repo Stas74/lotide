d67fb7d30f4a272b0cf8892fe5de6bdd95f46175
Stas74 <splotnikov74@gmail.com>
2022-01-17 19:58:19 -0700

    Remaking file to use chai assert

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,19 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
 
+describe("#middle", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it('returns ["camel", "duck"] for ["bison", "camel", "duck", "elephant"]', () => {    
+    assert.deepEqual(middle(["bison", "camel", "duck", "elephant"]), ["camel", "duck"]); 
+  });  
+  it('returns [] for ["bison", "camel"]', () => {
+    assert.deepEqual(middle(["bison", "camel"]), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(middle(["camel"]), []);
+  });
 
-assertArraysEqual(middle(["bison", "camel", "duck", "elephant"]), ["camel", "duck"]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle(["bison", "camel"]), []);
-assertArraysEqual(middle(["camel"]), []);
+});
