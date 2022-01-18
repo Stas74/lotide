ee52b627c783583fa418c2effecc3ceea02fd901
Stas74 <splotnikov74@gmail.com>
2022-01-17 17:38:16 -0700

    Remove test code from file

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arrOne, arrTwo) {
   if (arrOne.length !== arrTwo.length) {
-    return false
+    return false;
   }
 
-  for (let i = 0; i < arrOne.length; i++) {    
+  for (let i = 0; i < arrOne.length; i++) {
     if (arrOne[i] !== arrTwo[i]) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
+module.exports = eqArrays;
