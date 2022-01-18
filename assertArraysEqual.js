f63303102b617cc601aff506349b4e868a9bdde7
Stas74 <splotnikov74@gmail.com>
2022-01-17 17:52:18 -0700

    Remove eqArrays function code from file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,16 @@
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false
-  }
+const eqArrays = require('./eqArrays');
 
-  for (let i = 0; i < arrOne.length; i++) {    
-    if (arrOne[i] !== arrTwo[i]) {
-      return false
-    }
-  }
-  return true
-}
-
-const  assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-    return true
+    return true;
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false
+    return false;
   }
-}
+};
 
-console.log(assertArraysEqual(['asd', 'qw'], ['asd', 'qwe']))
+module.exports = assertArraysEqual;
+
+assertArraysEqual(['asd', 'qwe'], ['asd', 'qwe']);
+assertArraysEqual(['asd', 'qw'], ['asd', 'qwe']);
